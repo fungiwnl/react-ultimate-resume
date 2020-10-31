5218e3b1202e0b2109df8c77eb629e711122ed1b
Brian Fung <brianf.iwnl@gmail.com>
2020-10-31 18:44:27 +1100

    Update footer tooltips

--- src/package/components/footer/share_links/share_links_data.jsx
+++ src/package/components/footer/share_links/share_links_data.jsx
@@ -2,7 +2,6 @@ import React from 'react';
 import { FormattedMessage } from 'react-intl';
 
 import { CopyWrapper } from '../../commons/copy_wrapper/copy_wrapper';
-// import { ReactComponent as TwitterIcon } from '../../../assets/icons/brands/twitter.svg';
 import { ReactComponent as GitHubIcon } from '../../../assets/icons/brands/github.svg';
 import { ReactComponent as LinkedInIcon } from '../../../assets/icons/brands/linkedin.svg';
 import { ReactComponent as ShareIcon } from '../../../assets/icons/share.svg';
