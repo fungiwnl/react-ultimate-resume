e2fefbbdaa0023200991f865466905fa27881d4e
Brian Fung <brianf.iwnl@gmail.com>
2021-07-18 22:11:59 +1000

    Add GitBook link to the footer

--- src/package/components/footer/share_links/share_links_data.jsx
+++ src/package/components/footer/share_links/share_links_data.jsx
@@ -3,6 +3,7 @@ import { FormattedMessage } from 'react-intl';
 
 import { CopyWrapper } from '../../commons/copy_wrapper/copy_wrapper';
 import { ReactComponent as GitHubIcon } from '../../../assets/icons/brands/github.svg';
+import { ReactComponent as GitBookIcon } from '../../../assets/icons/brands/gitbook.svg';
 import { ReactComponent as LinkedInIcon } from '../../../assets/icons/brands/linkedin.svg';
 import { ReactComponent as ShareIcon } from '../../../assets/icons/share.svg';
 
@@ -17,6 +18,19 @@ export const SHARE_LINKS_DATA = Object.freeze({
             />
         ),
         getLink: ({ gitHubLink }) => `https://github.com/${gitHubLink}`
+    },
+    gitBook: {
+        icon: GitBookIcon,
+        tooltipTranslation: (
+            <FormattedMessage
+                id="Footer.shareLinks.shareOnTooltip"
+                defaultMessage="My {platform}"
+                values={{ platform: 'Gitbook' }}
+            />
+        ),
+        getLink: () => {
+            return `https://wiki.b-fung.com/`;
+        }
     },
     linkedIn: {
         icon: LinkedInIcon,
